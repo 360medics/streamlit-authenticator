4de88c3fdd082c580f61d08444fde9e17325feb6
lgabilly <louis.gabilly@gmail.com>
2023-12-18 17:41:54 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="streamlit-authenticator",
-    version="0.2.3",
+    version="0.3.0",
     author="Mohammad Khorasani",
     author_email="khorasani.mohammad@gmail.com",
     description="""
